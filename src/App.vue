51b5b20539df908624890ee7ae32b17960a4b9ad
dymafr <erwan1309@hotmail.com>
2022-04-05 14:18:28 +0200

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <!--  OUVREZ le lien du projet à droite dans un nouvel onglet pour voir la modification de l'URL lors de la navigation   -->
-  <nav class="d-flex flex-row align-items p-20">
-    <router-link to="/a" class="mr-10"> Page A</router-link>
-    <router-link class="mr-10" to="/b"> Page B</router-link>
-  </nav>
-  <div class="d-flex flex-row justify-content-center">
-    <router-view> </router-view>
+  <div class="container">
+    <nav class="d-flex flex-row align-items p-20">
+      <router-link to="/a" class="mr-10"> Page A</router-link>
+      <router-link class="mr-10" to="/b"> Page B</router-link>
+    </nav>
+    <div class="d-flex flex-row justify-content-center">
+      <router-view> </router-view>
+    </div>
   </div>
 </template>
 
@@ -16,9 +18,15 @@
 
 nav {
   position: fixed;
+  top: 0px;
+  width: 100%;
   background-color: var(--primary-1);
   a {
     color: white;
   }
+}
+
+.container {
+  margin-top: 60px;
 }
 </style>
