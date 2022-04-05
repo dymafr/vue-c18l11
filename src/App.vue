dae9c588b33bdad3b7083e672f5c6c8c7ffe87c0
dymafr <erwan1309@hotmail.com>
2022-04-05 14:22:00 +0200

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <div class="container">
     <nav class="d-flex flex-row align-items p-20">
       <router-link to="/a" class="mr-10"> Page A</router-link>
+      <router-link to="/a#myp" class="mr-10"> My p</router-link>
       <router-link class="mr-10" to="/b"> Page B</router-link>
     </nav>
     <div class="d-flex flex-row justify-content-center">
--- src/PageA.vue
+++ src/PageA.vue
@@ -58,7 +58,7 @@
       1500s, when an unknown printer took a galley of type and scrambled it to
       Ipsum.
     </p>
-    <p>
+    <p id="myapp" style="color: red">
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry's standard dummy text ever since the
       1500s, when an unknown printer took a galley of type and scrambled it to
